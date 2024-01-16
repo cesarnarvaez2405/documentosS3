d80cb2ae2befdbc6b7f08a4b17aa9d6d09eef85a
Cesar Narvaez <cesar.teacher24@gmail.com>
2024-01-16 00:03:24 -0500

    Se realiza guardado de archivos en la carpeta local

--- src/docs/docs.controller.ts
+++ src/docs/docs.controller.ts
@@ -9,19 +9,31 @@ import {
 } from '@nestjs/common';
 import { DocsService } from './docs.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
 
 @Controller('docs')
 export class DocsController {
   constructor(private readonly docsService: DocsService) {}
 
   @Post()
-  @UseInterceptors(FileInterceptor('file'))
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: function (req, file, cb) {
+          cb(null, './tmp/archivos-porta');
+        },
+        filename: function (req, file, cb) {
+          cb(null, file.originalname + '_' + Date.now() + '.png');
+        },
+      }),
+    }),
+  )
   async uploadFile(
     @UploadedFile(
       new ParseFilePipe({
         validators: [
           // new MaxFileSizeValidator({ maxSize: 1000 }),
-          // new FileTypeValidator({ fileType: 'image/png' }),
+          new FileTypeValidator({ fileType: 'image/png' }),
         ],
       }),
     )
